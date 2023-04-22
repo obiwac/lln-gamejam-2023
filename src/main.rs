dacc4e226113ae01009af0242d5a0deec9366915
obiwac <obiwac@gmail.com>
2023-04-23 01:09:30 +0200

    png: Draw png

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,10 @@ fn main() -> Result<(), Box<dyn Error>> {
 	const WIDTH: u32 = 800;
 	const HEIGHT: u32 = 600;
 
-	let png = aqua::png::Png::from_path("res/pig.png");
+	let mut png = aqua::png::Png::from_path("res/pig.png");
+	let mut png_result = png.draw();
+
+	println!("{:} {:}", png_result.width, png_result.height);
 
 	return Ok(());
 
